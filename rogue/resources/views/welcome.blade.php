3adefba3ec20e72bd3a8486d5eb115fea2dedd88
immaroot <killiankelly@gmail.com>
2020-02-05 16:26:03 -0500

    basic laravel environment with user authentication for issue #16 and #10

---
+++ rogue/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="jumbotron">
+                <div class="display-4">Welcome to Rogue!</div>
+                <p class="lead">This is a project for SOEN341. You can register and start posting and following other users.</p>
+            </div>
+        </div>
+    </div>
+@endsection
