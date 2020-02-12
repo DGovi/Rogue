527d1b4fb45586d1be26dee9020fdef93e1851c5
immaroot <killiankelly@gmail.com>
2020-02-12 13:39:52 -0500

    fixed a couple of issues that broke on my side when committing issue #7 and #22

--- rogue/app/Http/Controllers/CommentsController.php
+++ rogue/app/Http/Controllers/CommentsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use App\User;
 use Illuminate\Http\Request;
 use App\Comment;
 use Illuminate\Support\Facades\Auth;
@@ -11,10 +13,20 @@ class CommentsController extends Controller
     public function store(Request $request){
         $comment = new Comment();
         $user = User::findOrFail(Auth::id());
+        $post = Post::findOrFail($request->post_id);
+
+        $comment->user_id = $user->id;
+        $comment->post_id = $post->id;
 
         $request->validate([
             'comment' => 'required',
         ]);
+
+        $comment->comment = $request->comment;
+        $comment->save();
+
+        return redirect('posts/'.$post->id);
+
     }
 
 
