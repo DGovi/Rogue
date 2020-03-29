be8a69dc95a6e38d76bdcdacd3789be0121dbab4
Ashraf Khalil <ashrafdude@gmail.com>
2020-03-29 13:25:16 -0400

    removed text box for choose file buuton, and centered input fields in edit profile page

--- rogue/resources/views/profiles/edit.blade.php
+++ rogue/resources/views/profiles/edit.blade.php
@@ -12,7 +12,7 @@
                         @csrf
                         @method('PATCH')
 
-                        <div class="form-group row">
+                        <div style="position:relative; left:-55px;"class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">Username</label>
 
                             <div class="col-md-6">
@@ -26,7 +26,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div style="position: relative; left:-55px;" class="form-group row">
                             <label for="title" class="col-md-4 col-form-label text-md-right">Title</label>
 
                             <div class="col-md-6">
@@ -41,10 +41,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="image" class="col-md-4 col-form-label text-md-right">Profile Photo</label>
+                            <label style="position:relative; left:-54px;" for="image" class="col-md-4 col-form-label text-md-right">Profile Photo</label>
 
                             <div class="col-md-6">
-                                <input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image">
+                                <input id="image" type="file"style="border:0px;position: relative;align-self: center; right:-14px"class="form-control @error('image') is-invalid @enderror" name="image">
 
                                 @error('image')
                                 <span class="invalid-feedback" role="alert">
@@ -55,7 +55,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div style="position: center;left: 14px;"class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     Update Profile
                                 </button>
