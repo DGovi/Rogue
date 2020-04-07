99cc90c9636afa9556eba457325f31b8217722c9
SekayZ <44130679+SekayZ@users.noreply.github.com>
2020-04-07 15:52:16 -0400

    Add files via upload

--- rogue/tests/Unit/UserTest.php
+++ rogue/tests/Unit/UserTest.php
@@ -16,6 +16,7 @@ class UserTest extends TestCase
 {
     use RefreshDatabase;
 
+    // @@@ OK
     public function test_as_a_guest_I_can_register()
     {
         $user = factory(User::class)->create();
@@ -24,6 +25,7 @@ class UserTest extends TestCase
         $response->assertRedirect('/');
     }
 
+    // @@@ OK
     public function test_as_a_user_I_can_login()
     {
         $user = factory(User::class)->create();
@@ -32,18 +34,89 @@ class UserTest extends TestCase
         $response->assertRedirect('/');
     }
 
+    // *** TESTING FOLLOWING *** //
+
+    // @@@ OK
+    public function test_as_a_user_I_can_follow_someone_else()
+    {
+        $user = factory(User::class)->create();
+        $user_to_follow = factory(User::class)->create();
+       
+        $response = $this->actingAs($user)
+            ->post('follow', [
+                'follow' => $user_to_follow->id,
+            ]);
+        
+        $response->assertStatus(302);
+        $response->assertRedirect('profile/'.$user_to_follow->id);
+    }
+
+    // @@@ OK
+    public function test_as_a_guest_I_cant_follow_someone_else()
+    {
+        $user_to_follow = factory(User::class)->create();
+       
+        $response = $this
+            ->post('follow', [
+                'follow' => $user_to_follow->id,
+            ]);
+        
+        $response->assertNotFound();
+    }
+
+    // *** TESTING COMMENTING *** //
+
+    // @@@ OK
+    public function test_as_a_user_I_can_comment_on_someone_elses_post()
+    {
+        $user = factory(User::class)->create();
+        $otherUser = factory(User::class)->create();
+        $post = $otherUser->posts()->save(factory(Post::class)->make());
+       
+        $response = $this->actingAs($user)
+            ->post('comments', [
+                'post_id' => $post->id,
+                'user_id' => $otherUser->id,
+                'comment' => 'blabla',
+            ]);
+        
+        $response->assertStatus(302);
+        $response->assertRedirect('posts/'.$post->id);
+    }
+
+    // @@@ OK
+    public function test_as_a_user_I_can_comment_on_my_own_post()
+    {
+        $user = factory(User::class)->create();
+        $post = $user->posts()->save(factory(Post::class)->make());
+       
+        $response = $this->actingAs($user)
+            ->post('comments', [
+                'post_id' => $post->id,
+                'user_id' => $user->id,
+                'comment' => 'blabla',
+            ]);
+       
+        $response->assertStatus(302);
+        $response->assertRedirect('posts/'.$post->id);
+    }
+
+    // *** TESTING POST CREATION *** //
+
+    // @@@ OK
     public function test_as_a_user_I_can_create_a_post()
     {
         Storage::fake('public');
         $user = factory(User::class)->create();
         $file = UploadedFile::fake()->image('new_post.jpg', 400 , 400);
-        //echo $file;
+
         $response = $this->actingAs($user)
             ->post('posts', [
                 'user_id' => $user->id,
                 'image' => $file,
-                'caption' => 'caption',
+                'caption' => 'blabla',
                 ]);
+
         $latest_post = Post::latest()->first();
         $this->assertEquals('images/posts/' . $file->hashName(), $latest_post->image);
         Storage::disk('public')->assertExists('images/posts/' . $file->hashName());
@@ -51,28 +124,102 @@ class UserTest extends TestCase
         $response->assertRedirect('posts/' . $latest_post->user->id);
     }
 
-    public function test_as_a_user_I_can_follow_someone_else()
+    // @@@ OK
+    public function test_as_a_user_I_cant_create_post_with_empty_caption()
     {
+        Storage::fake('public');
         $user = factory(User::class)->create();
-        $user_to_follow = factory(User::class)->create();
+        $file = UploadedFile::fake()->image('new_post.jpg', 400 , 400);
+
         $response = $this->actingAs($user)
-            ->post('follow', [
-                'follow' => $user_to_follow->id,
-            ]);
-        $response->assertStatus(302);
-        $response->assertRedirect('profile/'.$user_to_follow->id);
+            ->post('posts', [
+                'user_id' => $user->id,
+                'image' => $file,
+                'caption' => null,
+                ]);
+        
+        $response->assertSessionHasErrors(('caption'));
+        
     }
 
-    public function test_as_a_user_I_can_comment_on_someone_elses_post()
+    // @@@ OK
+    public function test_as_a_user_I_cant_create_post_with_empty_image()
+    {
+        Storage::fake('public');
+        $user = factory(User::class)->create();
+        $file = UploadedFile::fake()->image('new_post.jpg', 400 , 400);
+    
+        $response = $this->actingAs($user)
+            ->post('posts', [
+                'user_id' => $user->id,
+                'image' => null,
+                'caption' => 'blabla',
+                ]);
+        
+        $response->assertSessionHasErrors(('image'));
+    }
+
+    // @@@ OK
+    public function test_as_a_user_I_cant_create_post_with_all_empty_fields()
+    {
+        Storage::fake('public');
+        $user = factory(User::class)->create();
+        $file = UploadedFile::fake()->image('new_post.jpg', 400 , 400);
+    
+        $response = $this->actingAs($user)
+            ->post('posts', [
+                'user_id' => $user->id,
+                'image' => null,
+                'caption' => null,
+                ]);
+        
+        $response->assertSessionHasErrors(('image'));
+        $response->assertSessionHasErrors(('caption'));
+    }
+
+
+    // *** TESTING SEARCH BAR *** //
+
+    /* to fix
+    Failed asserting that two strings are equal.
+    --- Expected
+    +++ Actual
+    @@ @@
+    -'http://localhost/profile/2'
+    +'http://localhost/profile'
+    */
+    public function test_as_a_user_I_can_search_other_user()
     {
         $user = factory(User::class)->create();
-        $post = $user->posts()->save(factory(Post::class)->make());
+        $otherUser = factory(User::class)->create();
+
         $response = $this->actingAs($user)
-            ->post('comments', [
-                'post_id' => $post->id,
-                'comment' => 'blabla',
-            ]);
-        $response->assertStatus(302);
-        $response->assertRedirect('posts/'.$post->id);
+            ->post('search', [
+                'username' => $otherUser->username,
+                ]);
+
+        
+        $response->assertRedirect('profile/'.$otherUser->id);
     }
+
+    /*public function test_as_a_guest_I_can_search_other_user()
+    {
+
+    }*/
+
+    // @@@ OK
+    public function test_as_a_user_searching_for_non_existing_user()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->post('search', [
+                'user_id' => null,
+                ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('profile/');
+    }
+
+
 }
